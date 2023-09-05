a9ac033c2a510b6fb7669c3111b522f027dfa33d
Harsh Rajput <78521904+rajput-code@users.noreply.github.com>
2023-09-05 14:49:31 +0530

    Update aws_proejct.py

--- aws_proejct.py
+++ aws_proejct.py
@@ -3,28 +3,27 @@ import boto3
 # Initialize the EC2 client
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
-# Create an EC2 instance
 response = ec2.run_instances(
     ImageId='ami-051f7e7f6c2f40dc1',  
     InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1,
     KeyName='harsh',  
-    SecurityGroupIds=['sg-0406ef29c0b4a5ae4'],  # Replace with your security group ID
+    SecurityGroupIds=['sg-0406ef29c0b4a5ae4'],  
 )
 
-# Wait for the instance to be running
+
 instance_id = response['Instances'][0]['InstanceId']
 waiter = ec2.get_waiter('instance_running')
 waiter.wait(InstanceIds=[instance_id])
 
-public_ip = '34.197.28.120'  # Replace with your Elastic IP address
+public_ip = '34.197.28.120' 
 ec2.associate_address(InstanceId=instance_id, PublicIp=public_ip)
 
-s3 = boto3.client('s3', region_name='us-east-1')  # Replace 'us-east-1' with your preferred region
+s3 = boto3.client('s3', region_name='us-east-1') 
 
-# Specify the bucket name (must be globally unique within the specified region)
+
 bucket_name = 'bucket-198'
 
-# Create the S3 bucket
+
 s3.create_bucket(Bucket=bucket_name)
